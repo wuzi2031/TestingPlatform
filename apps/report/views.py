0194626112605ac533800ade96716aec915ccacd
zhimingwu <wuzhiming_cd@keruyun.com>
2018-10-26 17:28:30 +0800

    update

--- apps/report/views.py
+++ apps/report/views.py
@@ -29,8 +29,9 @@ class TaskReportViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = TestTask.objects.filter(Q(task_state='block') | Q(task_state='finish') | Q(task_state='stop'))
     serializer_class = TestTaskSerialaer
     pagination_class = Pagination
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_class = TaskFilter
+    search_fields = ('name',)  # 搜索字段
     permission_classes = (IsAuthenticated,)  # 登录验证
     authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)  # jwt验证
 
